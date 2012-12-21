aa65883f49ee0ed071b8295d5ac2be3c8d9adb08
John Ryan <jaryan@glgroup.com>
2012-12-21 16:43:10 +0000

    added durable to queue

--- lib/worker.rb
+++ lib/worker.rb
@@ -19,7 +19,7 @@ module RabbitHutch
     # begin listening for all topics in publish.#
     def start
       @exchange = @channel.topic(@exchange_name, :durable => true, :auto_delete => false, :internal => true)
-      @queue = @channel.queue(@queue_name, :durable => false, :auto_delete => false)
+      @queue = @channel.queue(@queue_name, :durable => true, :auto_delete => false)
       @queue.bind(@exchange, :routing_key => 'publish.#')
       @queue.subscribe(&@consumer.method(:handle_message))
     end
